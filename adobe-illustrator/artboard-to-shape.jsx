3fe01e018f9a950907c8057ef003bb18d6cbf115
HeroLeam <thiagoleam@gmail.com>
2023-08-20 14:01:37 -0300

    Update artboard-to-shape.jsx

--- adobe-illustrator/artboard-to-shape.jsx
+++ adobe-illustrator/artboard-to-shape.jsx
@@ -4,19 +4,15 @@ Copyright © 2023
 https://linktr.ee/heroleam
 */
 
-// Script: Cria uma forma com a cor selecionada no tamanho da artboard 2 cm maior
+// Script: Cria uma forma com a cor selecionada no tamanho da artboard
 
-// ----------------------------------------------------------------------------------- //
-
-// Seleciona o documento ativo
 var doc = app.activeDocument;
-// Seleciona a camada ativa
 var layer = doc.activeLayer;
 var artboard = doc.artboards[doc.artboards.getActiveArtboardIndex(1)];
 var centimetros = 28.35;
 
-var fundoLayer = doc.layers.add(); // Cria uma nova camada
-fundoLayer.name = "Fundo"; // Define o nome da camada como "Fundo"
+var fundoLayer = doc.layers.add();
+fundoLayer.name = "Fundo";
 var lightBlueColor = new RGBColor();
 lightBlueColor.red = 79;
 lightBlueColor.green = 128;
@@ -24,19 +20,14 @@ lightBlueColor.blue = 255;
 fundoLayer.color = lightBlueColor;
 fundoLayer.zOrder(ZOrderMethod.SENDTOBACK);
 
-// Cria o retângulo
-var width =
-  artboard.artboardRect[2] - artboard.artboardRect[0] + centimetros * 2; // adiciona 2cm à largura
-var height =
-  artboard.artboardRect[1] - artboard.artboardRect[3] + centimetros * 2; // adiciona 2cm à altura
+var width = artboard.artboardRect[2] - artboard.artboardRect[0] + centimetros;
+var height = artboard.artboardRect[1] - artboard.artboardRect[3] + centimetros;
 
-// Centraliza o retângulo
 var x = artboard.artboardRect[0] - (centimetros * 2) / 2;
 var y = artboard.artboardRect[1] + (centimetros * 2) / 2;
 var square = fundoLayer.pathItems.rectangle(y, x, width, height);
-square.filled = true; // Preenchimento
-square.stroked = false; // Contorno
-square.selected = true; // seleciona o retângulo criado
+square.filled = true;
+square.stroked = false;
+square.selected = true;
 
-// Ajusta a artboard no tamanho do retângulo criado
-doc.fitArtboardToSelectedArt(0); // ajusta a artboard no tamanho do retângulo criado
+doc.fitArtboardToSelectedArt(0);
