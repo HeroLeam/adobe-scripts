aec675a08542aba488507072d21e568ef55baa15
HeroLeam <92167805+HeroLeam@users.noreply.github.com>
2023-08-21 09:45:03 -0300

    Update pixel-count.jsx

--- adobe-photoshop/pixel-count.jsx
+++ adobe-photoshop/pixel-count.jsx
@@ -8,16 +8,13 @@ https://linktr.ee/heroleam
 
 var doc = app.activeDocument;
 var channel = doc.activeChannels[0];
-
 var histogram = channel.histogram;
-
 var totalPixelCount = 0;
+
 for (var i = 0; i < histogram.length; i++) {
   totalPixelCount += histogram[i];
 }
-
 var pixelCount = totalPixelCount - histogram[0];
-
 var d = new ActionDescriptor();
 d.putString(stringIDToTypeID("textData"), totalPixelCount);
 executeAction(stringIDToTypeID("textToClipboard"), d, DialogModes.NO);
