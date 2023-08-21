14f3f69162636b9f70b05350ecb477957a86a641
HeroLeam <92167805+HeroLeam@users.noreply.github.com>
2023-08-21 09:38:08 -0300

    Update artboard-to-shape.jsx

--- adobe-illustrator/artboard-to-shape.jsx
+++ adobe-illustrator/artboard-to-shape.jsx
@@ -4,28 +4,28 @@ Copyright © 2023
 https://linktr.ee/heroleam
 */
 
-// Script: Cria uma forma com a cor selecionada no tamanho da artboard
+// Creates a shape with the selected color in the artboard size
 
 var doc = app.activeDocument;
 var layer = doc.activeLayer;
 var artboard = doc.artboards[doc.artboards.getActiveArtboardIndex(1)];
-var centimetros = 28.35;
+var layerName = "Background"
 
-var fundoLayer = doc.layers.add();
-fundoLayer.name = "Fundo";
+var backgroundLayer = doc.layers.add();
+backgroundLayer.name = layerName;
 var lightBlueColor = new RGBColor();
 lightBlueColor.red = 79;
 lightBlueColor.green = 128;
 lightBlueColor.blue = 255;
-fundoLayer.color = lightBlueColor;
-fundoLayer.zOrder(ZOrderMethod.SENDTOBACK);
+backgroundLayer.color = lightBlueColor;
+backgroundLayer.zOrder(ZOrderMethod.SENDTOBACK);
 
-var width = artboard.artboardRect[2] - artboard.artboardRect[0] + centimetros;
-var height = artboard.artboardRect[1] - artboard.artboardRect[3] + centimetros;
+var width = artboard.artboardRect[2] - artboard.artboardRect[0];
+var height = artboard.artboardRect[1] - artboard.artboardRect[3];
 
-var x = artboard.artboardRect[0] - (centimetros * 2) / 2;
-var y = artboard.artboardRect[1] + (centimetros * 2) / 2;
-var square = fundoLayer.pathItems.rectangle(y, x, width, height);
+var x = artboard.artboardRect[0] / 2;
+var y = artboard.artboardRect[1] / 2;
+var square = backgroundLayer.pathItems.rectangle(y, x, width, height);
 square.filled = true;
 square.stroked = false;
 square.selected = true;
