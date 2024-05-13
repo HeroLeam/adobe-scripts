d4081c31719da1c37daa81f9173e346c1b6b4974
HeroLeam <92167805+HeroLeam@users.noreply.github.com>
2024-05-13 10:48:33 -0300

    Update artboardToShape.jsx

--- adobe-illustrator/artboardToShape.jsx
+++ adobe-illustrator/artboardToShape.jsx
@@ -1,32 +1,20 @@
 /*
 Author: Thiago Leoni Amaral
 Copyright © 2023
-https://heroleam.github.io/portfolio
+https://linktr.ee/heroleam
 */
 
 // Creates a shape with the selected color in the artboard size
 
 var doc = app.activeDocument;
-var layer = doc.activeLayer;
 var artboard = doc.artboards[doc.artboards.getActiveArtboardIndex(1)];
-var layerName = "Background"
-
-var backgroundLayer = doc.layers.add();
-backgroundLayer.name = layerName;
-var lightBlueColor = new RGBColor();
-lightBlueColor.red = 79;
-lightBlueColor.green = 128;
-lightBlueColor.blue = 255;
-backgroundLayer.color = lightBlueColor;
-backgroundLayer.zOrder(ZOrderMethod.SENDTOBACK);
 
 var x = artboard.artboardRect[0];
 var y = artboard.artboardRect[1];
-
 var width = artboard.artboardRect[2] - artboard.artboardRect[0];
 var height = artboard.artboardRect[1] - artboard.artboardRect[3];
 
-var square = backgroundLayer.pathItems.rectangle(y, x, width, height);
+var square = doc.pathItems.rectangle(y, x, width, height);
 square.filled = true;
 square.stroked = false;
 square.selected = true;
