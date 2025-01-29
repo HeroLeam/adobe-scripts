3962832ee5d28dd3f5912acbd2e9c0046ee2f51c
HeroLeam <92167805+HeroLeam@users.noreply.github.com>
2025-01-29 16:13:01 -0300

    Add files via upload

---
+++ adobe-illustrator/captionOrderFrames.jsx
@@ -0,0 +1,119 @@
+/*
+Author: Thiago Leoni Amaral
+Copyright © 2025
+https://heroleam.github.io/portfolio
+*/
+
+// Check if there is an open document
+if (app.documents.length === 0) {
+    app.documents.add();
+}
+
+var doc = app.activeDocument;
+var newLayer;
+
+// Check if the layer "Tabela de Cores" exists
+for (var i = 0; i < doc.layers.length; i++) {
+    if (doc.layers[i].name === "Tabela de Cores") {
+        newLayer = doc.layers[i];
+        break;
+    }
+}
+
+// If the layer does not exist, create it
+if (!newLayer) {
+    newLayer = doc.layers.add();
+    newLayer.name = "Tabela de Cores";
+    var lightBlueColor = new RGBColor();
+    lightBlueColor.red = 79;
+    lightBlueColor.green = 128;
+    lightBlueColor.blue = 255;
+    newLayer.color = lightBlueColor;
+}
+
+var cm = 28.35;
+
+// Function to create a rectangle
+function criarRetangulo(x, y, largura, altura, corRGB) {
+    var retangulo = doc.pathItems.rectangle(y, x, largura, altura);
+    retangulo.filled = true;
+    retangulo.fillColor = corRGB;
+    retangulo.stroked = false;
+    return retangulo;
+}
+
+// Function to create a text
+function criarTexto(conteudo, x, y, tamanhoFonte, alinhamento) {
+    var texto = doc.textFrames.add();
+    texto.contents = conteudo;
+    texto.top = y;
+    texto.left = x;
+    texto.textRange.size = tamanhoFonte;
+    texto.textRange.justification = alinhamento;
+    return texto;
+}
+
+// Colors for rectangles
+function criarCorRGB(r, g, b) {
+    var cor = new RGBColor();
+    cor.red = r;
+    cor.green = g;
+    cor.blue = b;
+    return cor;
+}
+
+// Creation of the main square
+var larguraPrincipal = 15 * cm;
+var alturaPrincipal = 16 * cm;
+var margemX = 100;
+var margemY = 700;
+
+// Groups all elements
+var grupoPrincipal = newLayer.groupItems.add();
+grupoPrincipal.name = "Tabela de Cores";
+
+var retanguloPrincipal = criarRetangulo(margemX, margemY, larguraPrincipal, alturaPrincipal, criarCorRGB(255, 255, 255));
+retanguloPrincipal.move(grupoPrincipal, ElementPlacement.PLACEATEND);
+
+// Internal coordinates
+var margemInternaX = margemX + 10;
+var margemInternaY = margemY + 10;
+var larguraColoracao = larguraPrincipal - 20;
+var alturaLinha = 1.5 * cm;
+var espacamento = 5;
+
+// Creating numbered lines
+var cores = [
+    criarCorRGB(255, 255, 255),
+    criarCorRGB(255, 255, 255),
+    criarCorRGB(255, 255, 255),
+    criarCorRGB(255, 255, 255),
+    criarCorRGB(255, 255, 255),
+    criarCorRGB(255, 255, 255),
+    criarCorRGB(255, 255, 255),
+    criarCorRGB(255, 255, 255),
+];
+
+for (var i = 0; i < 8; i++) {
+    var posY = margemInternaY - (i + 1) * (alturaLinha + espacamento);
+    var texto = criarTexto((i + 1) + ":", margemInternaX, posY - alturaLinha / 2 + -3, 40, Justification.LEFT);
+    var retangulo = criarRetangulo(margemInternaX + 30, posY, larguraColoracao - 30, alturaLinha, cores[i]);
+    texto.move(grupoPrincipal, ElementPlacement.PLACEATEND);
+    retangulo.move(grupoPrincipal, ElementPlacement.PLACEATEND);
+    retanguloPrincipal.move(grupoPrincipal, ElementPlacement.PLACEATEND);
+}
+
+// Center the group on the artboard
+var artboard = doc.artboards[doc.artboards.getActiveArtboardIndex()];
+var artboardBounds = artboard.artboardRect;
+var artboardCenterX = (artboardBounds[0] + artboardBounds[2]) / 2;
+var artboardCenterY = (artboardBounds[1] + artboardBounds[3]) / 2;
+
+var groupBounds = grupoPrincipal.visibleBounds;
+var groupCenterX = (groupBounds[0] + groupBounds[2]) / 2;
+var groupCenterY = (groupBounds[1] + groupBounds[3]) / 2;
+
+grupoPrincipal.position = [
+    grupoPrincipal.position[0] + (artboardCenterX - groupCenterX),
+    grupoPrincipal.position[1] + (artboardCenterY - groupCenterY)
+];
